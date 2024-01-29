1c07d7e76884e33bebbc50fd7cbdb993f5f7d0b3
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-28 23:05:56 -0500

    create User and Thought models

---
+++ models/Thought.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minlength: 1, maxlength: 280},
+  username: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  reactions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Reaction'
+  }],
+}, {
+  timestamps: true
+});
+
+// Create a virtual property `reactionCount` that gets the amount of reactions per post
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, },
+    thoughts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
+// Create a virtual property `friendCount` that gets the amount of friends per post
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
