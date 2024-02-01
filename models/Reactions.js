bc69bd6eb9f270a3bb0b709f802a197b37698c71
David Rodriguez <santiago1.dsrr@gmail.com>
2024-02-01 15:34:39 -0500

    added reactions schema

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose');
-const { STRING } = require('sequelize');
 
 const reactionSchema = new mongoose.Schema({
-    reactionId: { 
-        Type: mongoose.Schema.Types.ObjectId, 
-        default: () => new mongoose.Types.ObjectId()
-    },
-    reactionBody: {
-        Type: String, 
-        required: true,
-        maxlength: 280,
-    },
-    username: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Reaction = mongoose.model('Reaction', reactionSchema);
