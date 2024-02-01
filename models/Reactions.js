ec5a7595cbe2a619ab7a13a5782b4c8e66dd4fc3
David Rodriguez <santiago1.dsrr@gmail.com>
2024-02-01 15:03:12 -0500

    added reations model

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
+const { STRING } = require('sequelize');
 
 const reactionSchema = new mongoose.Schema({
-    
+    reactionId: { 
+        Type: mongoose.Schema.Types.ObjectId, 
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        Type: String, 
+        required: true,
+        maxlength: 280,
+    },
+    username: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const Reaction = mongoose.model('Reaction', reactionSchema);
