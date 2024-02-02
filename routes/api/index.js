c72dc4a5f750b1ca53414d3ae7a7d11749f6cb42
David Rodriguez <santiago1.dsrr@gmail.com>
2024-02-02 01:52:00 -0500

    added friends and reactions routes files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
+const reactionRoutes = require(`./reactionRoutes`);
+const friendRoutes = require(`./friendsRoutes`)
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
+router.use('/friends', friendRoutes);
+router.use('/reactions', reactionRoutes);
 
 module.exports = router;
